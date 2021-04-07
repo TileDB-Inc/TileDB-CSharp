ce4fed02e59437aeef07fed1f4d2f4e7328bd9b3
bdeng.xt@gmail.com <bdeng@DESKTOP-NT5F11P.localdomain>
2021-04-07 14:32:34 -0400

    added benchmark project

---
+++ csharp/TileDB_CSharp/BenchmarkTileDB_CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkTileDB_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //run benchmarks for dense array
+            BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchmarkDenseArray>();
+
+            //run benchmarks for sparse array
+            BenchmarkDotNet.Running.BenchmarkRunner.Run<BenchmarkSparseArray>();
+        }
+    }
+}
