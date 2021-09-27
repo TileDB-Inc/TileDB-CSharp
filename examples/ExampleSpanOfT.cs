b409d22a8b66eb08b01f6209ed596f228a0e6c3d
bdeng-xt <bdeng.xt@gmail.com>
2021-09-27 18:00:27 -0400

    fix the class name error

--- examples/ExampleSpanOfT.cs
+++ examples/ExampleSpanOfT.cs
@@ -29,7 +29,7 @@ using System.Collections.Generic;
 
 namespace TileDB.Example
 {
-    public class ExampleSystemArray
+    public class ExampleSpanOfT
     {
         public static void Main(string[] args)
         {
