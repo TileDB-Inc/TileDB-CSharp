39b509f17cf5cce24906d3245ce400890846f27b
bdeng-xt <bdeng.xt@gmail.com>
2021-12-09 15:44:29 -0500

    change github scripts

--- .github/scripts/install_tiledb_linux.sh
+++ .github/scripts/install_tiledb_linux.sh
@@ -1,2 +1,2 @@
-curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.3.0/tiledb-linux-2.3.0-a87da7f-full.tar.gz \
+curl --location -o tiledb.tar.gz https://github.com/TileDB-Inc/TileDB/releases/download/2.4.4/tiledb-linux-x86_64-2.4.4-7257605.tar.gz \
 && sudo tar -C /usr/local -xf tiledb.tar.gz
--- .github/scripts/run_example_linux.sh
+++ .github/scripts/run_example_linux.sh
@@ -1,4 +1,4 @@
 set -e -x
-sudo ldconfig /home/runner/work/TileDB-CSharp/TileDB-CSharp/dist/Linux/lib
+sudo ldconfig /home/runner/work/TileDB-CSharp/TileDB-CSharp/TileDB.CSharp/runtimes/linux-x64/native
 cd examples/TileDB.Example/bin/x64/Release/net5.0
 dotnet TileDB.Example.dll
