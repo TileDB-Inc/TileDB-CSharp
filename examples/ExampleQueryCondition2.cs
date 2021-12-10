e66408198c4a5731147ec71fd746490234d74214
bin.deng <76831442+bdeng-xt@users.noreply.github.com>
2021-12-10 11:05:33 -0500

    Add an interactive notebook demo (#52)
    * add an interactive notebook for demo
    
    * add nuget package manager as suggested vscode extension
    
    * a

--- examples/ExampleQueryCondition2.cs
+++ examples/ExampleQueryCondition2.cs
@@ -150,69 +150,83 @@ namespace TileDB.Example
         {
             TileDB.Context ctx = new TileDB.Context();
 
+            string[] string_list = new string[10]
+            {
+                "a","bb","ccc","dd",
+                "eee","f","g","hhh",
+                "i","jjj"
+            };
+
             //open array for read
             TileDB.Array array = new TileDB.Array(ctx, array_uri_, TileDB.QueryType.TILEDB_READ);
-            //query
-            TileDB.Query query = new TileDB.Query(ctx, array, TileDB.QueryType.TILEDB_READ);
 
-            TileDB.VectorInt32 subarray = new TileDB.VectorInt32(new int[4] { 1, 2, 2, 4 });
+            TileDB.VectorInt32 subarray = new TileDB.VectorInt32(new int[4] { 1, 4, 1, 4 });
 
-            //from the subarray, we know the maximum number of elements is 6(2x3), so we initialize the buffer with length of 6  
+            //from the subarray, we know the maximum number of elements is 16(4x4), so we initialize the buffer with length of 6  
             TileDB.TileDBBuffer<int> tdb_int_buffer = new TileDBBuffer<int>();
-            tdb_int_buffer.Init(6, false, false);
+            tdb_int_buffer.Init(16, false, false);
 
-            //Here we initialize the string buffer to have maxium number of string as 6, for each string might have several characters
+            //Here we initialize the string buffer to have maxium number of string as 16, for each string might have several characters
             //so we use 100 as buffer size for char
             TileDB.TileDBBuffer<string> tdb_str_buffer = new TileDBBuffer<string>();
-            tdb_str_buffer.Init(6, true, false, 100);
+            tdb_str_buffer.Init(16, true, false, 100);
+
+
 
             TileDB.QueryStatus status = TileDB.QueryStatus.TILEDB_UNINITIALIZED;
             TileDB.MapStringVectorUInt64 buffer_elements = new TileDB.MapStringVectorUInt64();
-            try
+
+            for (int i=0; i<string_list.Length;++i) 
             {
-                query.set_int32_subarray(subarray);
-                query.set_layout(TileDB.LayoutType.TILEDB_ROW_MAJOR);
-                query.set_buffer("a1", tdb_int_buffer.DataIntPtr, tdb_int_buffer.BufferSize, tdb_int_buffer.ElementDataSize);
-                query.set_buffer_with_offsets("a2", tdb_str_buffer.DataIntPtr, tdb_str_buffer.BufferSize, tdb_str_buffer.ElementDataSize, tdb_str_buffer.Offsets);
 
-                TileDB.QueryCondition qc1 = TileDB.QueryCondition.create_for_datatype(ctx, TileDB.DataType.TILEDB_INT32, "a1", "3", TileDB.QueryConditionOperatorType.TILEDB_GE);
-                TileDB.QueryCondition qc2 = TileDB.QueryCondition.create_for_datatype(ctx, TileDB.DataType.TILEDB_INT32, "a1", "7", TileDB.QueryConditionOperatorType.TILEDB_LT);
-                TileDB.QueryCondition qc3 = TileDB.QueryCondition.create_for_datatype(ctx, TileDB.DataType.TILEDB_STRING_ASCII, "a2", "dd", TileDB.QueryConditionOperatorType.TILEDB_EQ);
-                TileDB.QueryCondition qc12 = qc1.combine(qc2, TileDB.QueryConditionCombinationOperatorType.TILEDB_AND);
-                TileDB.QueryCondition qc = qc12.combine(qc3, TileDB.QueryConditionCombinationOperatorType.TILEDB_AND);
-                //set combined condition for a1 >=3 and <7, for a2 =="dd"
-                query.set_condition(qc);
+                try
+                {
+                    //query
+                    TileDB.Query query = new TileDB.Query(ctx, array, TileDB.QueryType.TILEDB_READ);
+                    query.set_int32_subarray(subarray);
+                    query.set_layout(TileDB.LayoutType.TILEDB_ROW_MAJOR);
+                    query.set_buffer("a1", tdb_int_buffer.DataIntPtr, tdb_int_buffer.BufferSize, tdb_int_buffer.ElementDataSize);
+                    query.set_buffer_with_offsets("a2", tdb_str_buffer.DataIntPtr, tdb_str_buffer.BufferSize, tdb_str_buffer.ElementDataSize, tdb_str_buffer.Offsets);
 
-                status = query.submit();
-                buffer_elements = query.result_buffer_elements();
-                array.close();
+ 
+                    TileDB.QueryCondition qc = TileDB.QueryCondition.create_for_datatype(ctx, TileDB.DataType.TILEDB_STRING_ASCII, "a2", string_list[i], TileDB.QueryConditionOperatorType.TILEDB_EQ);
+                    //set combined condition for a1 >=3 and <7, for a2 =="dd"
+                    query.set_condition(qc);
 
+                    status = query.submit();
+                    buffer_elements = query.result_buffer_elements();
+                    
+
+                }
+                catch (TileDB.TileDBError tdbe)
+                {
+                    System.Console.WriteLine(tdbe.Message);
+                     
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    
+                }
+ 
+                //a1 int result
+                UInt64 result_el_a1_size = buffer_elements["a1"][1];
+                var a1_int = tdb_int_buffer.Data.Take((int)result_el_a1_size);
+
+                // a2 string query result
+                UInt64 result_el_a2_size = buffer_elements["a2"][1];
+                string[] a2_str = tdb_str_buffer.UnPackStringArray((int)result_el_a2_size);
+
+
+                //print out 
+                System.Console.WriteLine("query result a1:{0}, a2:{1}", String.Join(" ", a1_int), String.Join(" ", a2_str));
+
+                
             }
-            catch (TileDB.TileDBError tdbe)
-            {
-                System.Console.WriteLine(tdbe.Message);
-                return status;
-            }
-            catch (System.Exception e)
-            {
-                System.Console.WriteLine(e.Message);
-                return status;
-            }
+            array.close();
 
-            //a1 int result
-            UInt64 result_el_a1_size = buffer_elements["a1"][1];
-            var a1_int = tdb_int_buffer.Data.Take((int)result_el_a1_size);
-          
-            // a2 string query result
-            UInt64 result_el_a2_size = buffer_elements["a2"][1];
-            string[] a2_str = tdb_str_buffer.UnPackStringArray((int)result_el_a2_size);
-            
-  
-            //print out 
-            System.Console.WriteLine("query result a1:{0}", String.Join(" ", a1_int));
-            System.Console.WriteLine("query result a2:{0}", String.Join(" ", a2_str));
 
-            return status;
+            return TileDB.QueryStatus.TILEDB_COMPLETED;
         }//private TileDB.QueryStatus ReadArray()
         #endregion
 
