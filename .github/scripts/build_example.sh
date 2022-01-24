746ca0a1a1f81c6c9e2cc88da1e25f3ef21a42f5
Isaiah Norton <isaiah@tiledb.com>
2022-01-24 18:07:49 -0500

    Update CI

--- .github/scripts/build_example.sh
+++ .github/scripts/build_example.sh
@@ -1,3 +1,3 @@
 set -e -x
-cd TileDB-CSharp-Examples
+cd examples/TileDB-CSharp-Examples
 dotnet build /p:Platform=x64 -c Release
--- .github/scripts/run_example_linux.sh
+++ .github/scripts/run_example_linux.sh
@@ -1,3 +1,3 @@
 set -e -x
-cd TileDB-CSharp-Examples/bin/x64/Release/net6.0
+cd examples/TileDB-CSharp-Examples/bin/x64/Release/net6.0
 dotnet TileDB-CSharp-Examples.dll
