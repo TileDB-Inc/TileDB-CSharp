d0cac1a8a181a3605b36b314cb22c8dcbf61ba5e
Shaun M Reed <shaunrd0@gmail.com>
2022-08-04 12:49:09 -0400

    Update to TileDB 2.10.3 (#82)

--- .github/scripts/download_tiledb.sh
+++ .github/scripts/download_tiledb.sh
@@ -1,12 +1,18 @@
 set -e -x
-wget https://github.com/TileDB-Inc/TileDB/releases/download/2.10.2/tiledb-windows-x86_64-2.10.2-9ab84f9.zip
-unzip tiledb-windows-x86_64-2.10.2-9ab84f9.zip -d tiledb-windows
+TAG=2.10.3
+ID=7a5d1cd
+
+RELEASE=x86_64-$TAG-$ID
+wget https://github.com/TileDB-Inc/TileDB/releases/download/$TAG/tiledb-windows-$RELEASE.zip
+unzip tiledb-windows-$RELEASE.zip -d tiledb-windows
 cp ./tiledb-windows/bin/tiledb.dll ./sources/TileDB.CSharp/runtimes/win-x64/native/
-wget https://github.com/TileDB-Inc/TileDB/releases/download/2.10.2/tiledb-linux-x86_64-2.10.2-9ab84f9.tar.gz
+
+wget https://github.com/TileDB-Inc/TileDB/releases/download/$TAG/tiledb-linux-$RELEASE.tar.gz
 mkdir tiledb-linux
-tar xvfz tiledb-linux-x86_64-2.10.2-9ab84f9.tar.gz --directory tiledb-linux
-cp ./tiledb-linux/lib/libtiledb.so.2.10 ./sources/TileDB.CSharp/runtimes/linux-x64/native/
-wget https://github.com/TileDB-Inc/TileDB/releases/download/2.10.2/tiledb-macos-x86_64-2.10.2-9ab84f9.tar.gz
+tar xvfz tiledb-linux-$RELEASE.tar.gz --directory tiledb-linux
+cp ./tiledb-linux/lib/libtiledb.so.${TAG%.*} ./sources/TileDB.CSharp/runtimes/linux-x64/native/
+
+wget https://github.com/TileDB-Inc/TileDB/releases/download/$TAG/tiledb-macos-$RELEASE.tar.gz
 mkdir tiledb-macos
-tar xvfz tiledb-macos-x86_64-2.10.2-9ab84f9.tar.gz --directory tiledb-macos
+tar xvfz tiledb-macos-$RELEASE.tar.gz --directory tiledb-macos
 cp ./tiledb-macos/lib/libtiledb.dylib ./sources/TileDB.CSharp/runtimes/osx-x64/native/
