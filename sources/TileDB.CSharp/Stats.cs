c78b498bdcfd1d6ff6c58670c49e88faaf84050e
Shaun Reed <shaunrd0@gmail.com>
2022-09-07 12:43:06 -0400

    Changes from sync

--- sources/TileDB.CSharp/Stats.cs
+++ sources/TileDB.CSharp/Stats.cs
@@ -16,10 +16,12 @@ namespace TileDB.CSharp
         /// Enable statistic gathering
         /// </summary>
         public static void Enable() => _ctx.handle_error(Methods.tiledb_stats_enable());
+
         /// <summary>
         /// Disable statistic gathering
         /// </summary>
         public static void Disable() => _ctx.handle_error(Methods.tiledb_stats_disable());
+
         /// <summary>
         /// Reset all statistics previously gathered
         /// </summary>
@@ -28,24 +30,26 @@ namespace TileDB.CSharp
         /// <summary>
         /// Dump TileDB statistics to stdout
         /// </summary>
-        public static void Dump()
-        {
-            var ms = (sbyte**)Marshal.StringToHGlobalAnsi("");
-            _ctx.handle_error(Methods.tiledb_stats_dump_str(ms));
-            Console.WriteLine(new string(ms[0]));
-            Marshal.FreeHGlobal((IntPtr)ms);
-        }
+        public static void Dump() => Console.WriteLine(Get());
 
         /// <summary>
         /// Dump TileDB statistics to a file
         /// </summary>
-        /// <param name="filePath"></param>
-        public static void Dump(string filePath)
+        /// <param name="filePath">Path to file to dump statistics</param>
+        public static void Dump(string filePath) => System.IO.File.WriteAllText(filePath, Get());
+
+        /// <summary>
+        /// Get TileDB statistics as string
+        /// </summary>
+        /// <returns>string object containing dumped TileDB statistics</returns>
+        public static string Get()
         {
             var ms = (sbyte**)Marshal.StringToHGlobalAnsi("");
             _ctx.handle_error(Methods.tiledb_stats_dump_str(ms));
-            System.IO.File.WriteAllText(filePath, new string(ms[0]));
+            string stats = new(*ms);
+            _ctx.handle_error(Methods.tiledb_stats_free_str(ms));
             Marshal.FreeHGlobal((IntPtr)ms);
+            return stats;
         }
     }
 }
