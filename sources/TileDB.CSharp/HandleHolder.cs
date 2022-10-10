ef7b18e5120df1fb0c676621cbb1013fe813cb5a
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-10-10 14:08:02 +0300

    Add a handle holder.

---
+++ sources/TileDB.CSharp/HandleHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TileDB.CSharp
+{
+    internal struct HandleHolder<THandle, TNativeObject> : IDisposable where THandle : SafeHandle where TNativeObject : unmanaged
+    {
+        private readonly THandle _handle;
+        private bool _isHeld;
+
+#pragma warning disable S3869 // "SafeHandle.DangerousGetHandle" should not be called
+        public static unsafe implicit operator TNativeObject*(HandleHolder<THandle, TNativeObject> holder) =>
+            (TNativeObject*)holder._handle.DangerousGetHandle();
+#pragma warning restore S3869 // "SafeHandle.DangerousGetHandle" should not be called
+
+        public HandleHolder(THandle handle)
+        {
+            _handle = handle;
+            _isHeld = false;
+            _handle.DangerousAddRef(ref _isHeld);
+        }
+
+        public void Dispose()
+        {
+            if (!_isHeld)
+            {
+                return;
+            }
+
+            _isHeld = false;
+            _handle.DangerousRelease();
+        }
+    }
+}
