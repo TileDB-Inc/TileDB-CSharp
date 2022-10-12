ea62fa1ce1fcc20a4bc21b457d1fc86c9ff7e77b
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-10-12 10:46:43 +0300

    Fix errors in the `Context` class.

--- sources/TileDB.CSharp/SafeHandles/ContextHandle.cs
+++ sources/TileDB.CSharp/SafeHandles/ContextHandle.cs
@@ -6,29 +6,55 @@ namespace TileDB.Interop
 {
     internal unsafe class ContextHandle : SafeHandle
     {
-        public ContextHandle() : base(IntPtr.Zero, ownsHandle: true)
-        {
-            tiledb_ctx_t* context;
-            var hconfig = new ConfigHandle();
-            Methods.tiledb_ctx_alloc(hconfig, &context);
+        public ContextHandle() : base(IntPtr.Zero, true) { }
 
-            if (context == null)
+        public ContextHandle(IntPtr handle, bool ownsHandle) : base(IntPtr.Zero, ownsHandle) { SetHandle(handle); }
+
+        public static ContextHandle CreateUnowned(tiledb_ctx_t* schema) => new((IntPtr)schema, ownsHandle: false);
+
+        public static ContextHandle Create()
+        {
+            var handle = new ContextHandle();
+            bool successful = false;
+            tiledb_ctx_t* context = null;
+            try
             {
-                throw new Exception("Failed to allocate!");
+                using var configHandle = ConfigHandle.Create();
+                using var configHandleHolder = configHandle.Acquire();
+                Methods.tiledb_ctx_alloc(configHandleHolder, &context);
+                successful = true;
             }
-            InitHandle(context);
+            finally
+            {
+                if (successful)
+                {
+                    handle.InitHandle(context);
+                }
+            }
+
+            return handle;
         }
 
-        public ContextHandle(ConfigHandle hconfig) : base(IntPtr.Zero, ownsHandle: true)
+        public static ContextHandle Create(ConfigHandle configHandle)
         {
-            tiledb_ctx_t* context;
-            Methods.tiledb_ctx_alloc(hconfig, &context);
-
-            if (context == null)
+            var handle = new ContextHandle();
+            bool successful = false;
+            tiledb_ctx_t* context = null;
+            try
             {
-                throw new Exception("Failed to allocate!");
+                using var configHandleHolder = configHandle.Acquire();
+                Methods.tiledb_ctx_alloc(configHandleHolder, &context);
+                successful = true;
             }
-            InitHandle(context);
+            finally
+            {
+                if (successful)
+                {
+                    handle.InitHandle(context);
+                }
+            }
+
+            return handle;
         }
 
         protected override bool ReleaseHandle()
