8692cf23dd15cc73c58c9eb7c58e2bc62396ee69
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-10-12 12:04:40 +0300

    Fix errors in the `File` class.

--- sources/TileDB.CSharp/File.cs
+++ sources/TileDB.CSharp/File.cs
@@ -11,83 +11,89 @@ namespace TileDB.CSharp
     public sealed unsafe class File
     {
         private readonly Context _ctx;
-        
+
         public File(Context ctx)
         {
             _ctx = ctx;
         }
-        
+
         public ArraySchema SchemaCreate(string uri)
         {
+            using var ctxHandle = _ctx.Handle.Acquire();
             var ms_uri = new MarshaledString(uri);
             tiledb_array_schema_t* array_schema_p;
-            _ctx.handle_error(Methods.tiledb_filestore_schema_create(_ctx.Handle, ms_uri, &array_schema_p));
+            _ctx.handle_error(Methods.tiledb_filestore_schema_create(ctxHandle, ms_uri, &array_schema_p));
             if (array_schema_p == null)
             {
                 throw new ErrorException("Array.schema, schema pointer is null");
             }
-            return new ArraySchema(_ctx,array_schema_p);
+            return new ArraySchema(_ctx, ArraySchemaHandle.CreateUnowned(array_schema_p));
         }
-        
+
         public void URIImport(string arrayURI, string fileURI, MIMEType mimeType)
         {
+            using var ctxHandle = _ctx.Handle.Acquire();
             var ms_arrayURI = new MarshaledString(arrayURI);
             var ms_fileURI = new MarshaledString(fileURI);
             var tiledb_mime_type = (tiledb_mime_type_t)mimeType;
-            _ctx.handle_error(Methods.tiledb_filestore_uri_import(_ctx.Handle, ms_arrayURI, ms_fileURI, tiledb_mime_type));
-        }
-        
-        public void URIExport(string fileURI, string arrayURI)
-        {
-            var ms_fileURI = new MarshaledString(fileURI);
-            var ms_arrayURI = new MarshaledString(arrayURI);
-            _ctx.handle_error(Methods.tiledb_filestore_uri_export(_ctx.Handle, ms_fileURI, ms_arrayURI));
+            _ctx.handle_error(Methods.tiledb_filestore_uri_import(ctxHandle, ms_arrayURI, ms_fileURI, tiledb_mime_type));
         }
 
-        public void BufferImport<T>(string arrayURI, T value, ulong size, MIMEType mimeType) where T: struct
+        public void URIExport(string fileURI, string arrayURI)
         {
+            using var ctxHandle = _ctx.Handle.Acquire();
+            var ms_fileURI = new MarshaledString(fileURI);
+            var ms_arrayURI = new MarshaledString(arrayURI);
+            _ctx.handle_error(Methods.tiledb_filestore_uri_export(ctxHandle, ms_fileURI, ms_arrayURI));
+        }
+
+        public void BufferImport<T>(string arrayURI, T value, ulong size, MIMEType mimeType) where T : struct
+        {
+            using var ctxHandle = _ctx.Handle.Acquire();
             var ms_arrayURI = new MarshaledString(arrayURI);
             var data = new[] { value };
             var dataGcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
             var tiledb_mime_type = (tiledb_mime_type_t)mimeType;
             _ctx.handle_error(Methods.tiledb_filestore_buffer_import(
-                _ctx.Handle, 
-                ms_arrayURI, 
+                ctxHandle,
+                ms_arrayURI,
                 (void*)dataGcHandle.AddrOfPinnedObject(),
                 size,
                 tiledb_mime_type));
         }
-        
-        public T BufferExport<T>(string arrayURI, ulong offset, ulong size) where T: struct
+
+        public T BufferExport<T>(string arrayURI, ulong offset, ulong size) where T : struct
         {
+            using var ctxHandle = _ctx.Handle.Acquire();
             var ms_arrayURI = new MarshaledString(arrayURI);
             var data = new T[1];
             var dataGcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
             _ctx.handle_error(Methods.tiledb_filestore_buffer_export(
-                _ctx.Handle, 
-                ms_arrayURI, 
+                ctxHandle,
+                ms_arrayURI,
                 offset,
                 (void*)dataGcHandle.AddrOfPinnedObject(),
                 size));
-            
+
             var result = data[0];
             dataGcHandle.Free();
 
             return result;
         }
-        
+
         public ulong Size(string arrayURI)
         {
+            using var ctxHandle = _ctx.Handle.Acquire();
             var ms_arrayURI = new MarshaledString(arrayURI);
             ulong size;
             _ctx.handle_error(Methods.tiledb_filestore_size(
-                _ctx.Handle, 
+                ctxHandle,
                 ms_arrayURI,
                 &size));
 
             return size;
         }
-        
+
         public string MIMETypeToStr(MIMEType mimeType)
         {
             var tiledb_mime_type = (tiledb_mime_type_t)mimeType;
@@ -96,18 +102,18 @@ namespace TileDB.CSharp
             {
                 Methods.tiledb_mime_type_to_str(tiledb_mime_type, p_result);
             }
-            
+
             return ms_result;
         }
-        
+
         public MIMEType MIMETypeFromStr(string str)
         {
             tiledb_mime_type_t mimeType;
             var ms_str = new MarshaledString(str);
-            
+
             unsafe
             {
-                int status = TileDB.Interop.Methods.tiledb_mime_type_from_str(ms_str, &mimeType); 
+                int status = TileDB.Interop.Methods.tiledb_mime_type_from_str(ms_str, &mimeType);
                 if (status != (int)Status.TILEDB_OK)
                 {
                     throw new System.ArgumentException("MIMETypeFromStr, Invalid string:" + str);
