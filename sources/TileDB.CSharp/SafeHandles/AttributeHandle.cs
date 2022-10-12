4ada4be0d9f9b7e81f556c9f9e43c9f99841382d
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-10-12 10:32:42 +0300

    Add a forgotten line.

--- sources/TileDB.CSharp/SafeHandles/AttributeHandle.cs
+++ sources/TileDB.CSharp/SafeHandles/AttributeHandle.cs
@@ -22,6 +22,7 @@ namespace TileDB.Interop
                 using var contextHandle = context.Handle.Acquire();
                 var ms_name = new MarshaledString(name);
                 context.handle_error(Methods.tiledb_attribute_alloc(contextHandle, ms_name, datatype, &attribute));
+                successful = true;
             }
             finally
             {
