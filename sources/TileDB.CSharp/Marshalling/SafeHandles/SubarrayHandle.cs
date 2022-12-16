26d7a5715e313ce77faf9b685411002e8119e688
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-12-16 11:24:59 +0200

    Add `SubarrayHandle`.

---
+++ sources/TileDB.CSharp/Marshalling/SafeHandles/SubarrayHandle.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Runtime.InteropServices;
+using TileDB.Interop;
+
+namespace TileDB.CSharp.Marshalling.SafeHandles
+{
+    internal unsafe sealed class SubarrayHandle : SafeHandle
+    {
+        public SubarrayHandle() : base(IntPtr.Zero, true) { }
+
+        public SubarrayHandle(IntPtr handle, bool ownsHandle) : base(IntPtr.Zero, ownsHandle) { SetHandle(handle); }
+
+        public static SubarrayHandle Create(Context context, ArrayHandle arrayHandle)
+        {
+            var handle = new SubarrayHandle();
+            var successful = false;
+            tiledb_subarray_t* array = null;
+            try
+            {
+                using (var ctx = context.Handle.Acquire())
+                using (var arrayHandleHolder = arrayHandle.Acquire())
+                {
+                    context.handle_error(Methods.tiledb_subarray_alloc(ctx, arrayHandleHolder, &array));
+                }
+                successful = true;
+            }
+            finally
+            {
+                if (successful)
+                {
+                    handle.InitHandle(array);
+                }
+            }
+
+            return handle;
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            fixed (IntPtr* p = &handle)
+            {
+                Methods.tiledb_subarray_free((tiledb_subarray_t**)p);
+            }
+            return true;
+        }
+
+        internal void InitHandle(tiledb_subarray_t* h) { SetHandle((IntPtr)h); }
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        public SafeHandleHolder<tiledb_subarray_t> Acquire() => new(this);
+    }
+}
