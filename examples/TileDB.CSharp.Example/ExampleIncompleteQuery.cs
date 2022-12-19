4c1ec4f33a588429d2a033493cd2876fe72ad7fd
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2022-12-19 14:31:22 +0200

    Fix CI by removing one call to `SetSubarray` in the example.

--- examples/TileDB.CSharp.Example/ExampleIncompleteQuery.cs
+++ examples/TileDB.CSharp.Example/ExampleIncompleteQuery.cs
@@ -75,9 +75,6 @@ namespace TileDB.CSharp.Examples
                 arrayRead.Open(QueryType.Read);
                 var queryRead = new Query(Ctx, arrayRead);
                 queryRead.SetLayout(LayoutType.Unordered);
-                var subarray = new Subarray(arrayRead);
-                subarray.SetSubarray(1, 100, 1, 100);
-                queryRead.SetSubarray(subarray);
 
                 queryRead.SetDataBuffer("rows", rowsRead);
                 queryRead.SetDataBuffer("cols", colsRead);
