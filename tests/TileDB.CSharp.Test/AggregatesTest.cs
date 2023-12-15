5c6cd86a685726c800131eea8a605201b26c53f8
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-12-16 00:19:42 +0200

    Test null count aggregate.

--- tests/TileDB.CSharp.Test/AggregatesTest.cs
+++ tests/TileDB.CSharp.Test/AggregatesTest.cs
@@ -22,12 +22,14 @@ namespace TileDB.CSharp.Test
 
             array.Open(QueryType.Write);
 
-            int[] data = new int[10] { 62, 26, 81, 86, 93, 76, 60, 25, 68, 28 };
+            int[] data = new int[10] { 62, 26, 81, 86, 93, 0, 60, 25, 68, 28 };
+            byte[] validity = new byte[10] { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 };
 
             using (var query = new Query(array))
             {
                 query.SetLayout(LayoutType.RowMajor);
                 query.SetDataBuffer("a1", data);
+                query.SetValidityBuffer("a1", validity);
 
                 query.Submit();
             }
@@ -35,10 +37,12 @@ namespace TileDB.CSharp.Test
 
             array.Open(QueryType.Read);
             int[] dataRead = new int[data.Length];
-            ulong count;
+            byte[] validityRead = new byte[data.Length];
+            ulong count, nullCount;
             long sum;
             double mean;
             int min, max;
+            byte sumValidity, meanValidity, minValidity, maxValidity;
             using (var query = new Query(array))
             {
                 query.SetLayout(LayoutType.RowMajor);
@@ -52,6 +56,7 @@ namespace TileDB.CSharp.Test
                 channel.ApplyAggregate(AggregateOperation.Unary(AggregateOperator.Mean, "a1"), nameof(mean));
                 channel.ApplyAggregate(AggregateOperation.Unary(AggregateOperator.Min, "a1"), nameof(min));
                 channel.ApplyAggregate(AggregateOperation.Unary(AggregateOperator.Max, "a1"), nameof(max));
+                channel.ApplyAggregate(AggregateOperation.Unary(AggregateOperator.NullCount, "a1"), nameof(nullCount));
 
                 using var countField = query.GetField(nameof(count));
                 Assert.AreEqual(nameof(count), countField.Name);
@@ -60,20 +65,31 @@ namespace TileDB.CSharp.Test
                 Assert.AreEqual(QueryFieldOrigin.Aggregate, countField.Origin);
 
                 query.SetDataBuffer("a1", dataRead);
+                query.SetValidityBuffer("a1", validityRead);
                 query.SetDataBuffer(nameof(count), &count, 1);
                 query.SetDataBuffer(nameof(sum), &sum, 1);
+                query.SetValidityBuffer(nameof(sum), &sumValidity, 1);
                 query.SetDataBuffer(nameof(mean), &mean, 1);
+                query.SetValidityBuffer(nameof(mean), &meanValidity, 1);
                 query.SetDataBuffer(nameof(min), &min, 1);
+                query.SetValidityBuffer(nameof(min), &minValidity, 1);
                 query.SetDataBuffer(nameof(max), &max, 1);
+                query.SetValidityBuffer(nameof(max), &maxValidity, 1);
+                query.SetDataBuffer(nameof(nullCount), &nullCount, 1);
 
                 query.Submit();
 
                 CollectionAssert.AreEqual(data, dataRead);
+                CollectionAssert.AreEqual(validity, validityRead);
                 Assert.AreEqual((ulong)data.Length, count);
-                Assert.AreEqual(605, sum);
-                Assert.AreEqual(60.5, mean);
+                Assert.AreEqual(529, sum);
+                Assert.AreEqual(58.8, mean, 0.1);
                 Assert.AreEqual(25, min);
                 Assert.AreEqual(93, max);
+                Assert.AreEqual(1, sumValidity);
+                Assert.AreEqual(1, meanValidity);
+                Assert.AreEqual(1, minValidity);
+                Assert.AreEqual(1, maxValidity);
             }
         }
 
@@ -86,6 +102,7 @@ namespace TileDB.CSharp.Test
             array_schema.SetDomain(domain);
 
             using var a1 = new Attribute(context, "a1", DataType.Int32);
+            a1.SetNullable(true);
             array_schema.AddAttribute(a1);
             array_schema.Check();
             return array_schema;
