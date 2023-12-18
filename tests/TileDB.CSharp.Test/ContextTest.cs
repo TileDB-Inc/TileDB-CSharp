e0e6fe76856b5593b0f49f8216e17b1d8ea5743c
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-12-18 15:34:48 +0200

    Assert only that MemFS is supported. Fixes nightly build failures when the Core is not built with cloud backends enabled.

--- tests/TileDB.CSharp.Test/ContextTest.cs
+++ tests/TileDB.CSharp.Test/ContextTest.cs
@@ -58,10 +58,9 @@ namespace TileDB.CSharp.Test
         {
             using var ctx = new Context();
             Assert.IsTrue(ctx.IsFileSystemSupported(FileSystemType.InMemory));
-            Assert.IsTrue(ctx.IsFileSystemSupported(FileSystemType.S3));
-            Assert.IsTrue(ctx.IsFileSystemSupported(FileSystemType.Azure));
-            Assert.IsTrue(ctx.IsFileSystemSupported(FileSystemType.GoogleCloudStorage));
-            Assert.AreEqual(!OperatingSystem.IsWindows(), ctx.IsFileSystemSupported(FileSystemType.Hdfs));
+            // While the release binaries support all other filesystems (except for
+            // HDFS), binaries from nightly builds and other custom builds may not.
+            // MemFS is the only filesystem that is known to be always supported.
         }
 
         [TestMethod]
